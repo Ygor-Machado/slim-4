0a3e8b4cd2c05abd259246230e194eee527dd549
Ygor-Machado <ygaounderground011@gmail.com>
2023-12-02 04:02:43 -0300

    feat: Cria Classe para Flash Messages

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\controllers;
+use App\classes\Flash;
 use App\database\models\User;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -17,9 +18,15 @@ class Home extends Base
 
         $users = $this->user->find();
 
+        Flash::set('message', 'Cadastrado com sucesso');
+        $message = Flash::get('message');
+
+        var_dump($message);
+
         return $this->getTwig()->render($response, $this->setView('site/home'), [
             'title' => 'Home',
             'users'  => $users,
+            'message' => $message
         ]);
 
 
